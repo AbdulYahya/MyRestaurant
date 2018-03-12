7439ec2d79b3784642dbb27a643cff7b5ce301df
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-12 16:23:35 -0700

    Implemented first two android tests with Robolectric. Both tests passing

---
+++ app/src/test/java/me/ayahya/aesirr/myrestaurant/MainActivityTest.java
@@ -0,0 +1,45 @@
+package me.ayahya.aesirr.myrestaurant;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static android.os.Build.VERSION_CODES.O;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = O)
+
+public class MainActivityTest {
+    private MainActivity mainActivity;
+
+    @Before
+    public void setup() {
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() throws Exception {
+        TextView appNameTextView = mainActivity.findViewById(R.id.mainHeaderTextView);
+        assertThat(appNameTextView.getText().toString(), equalTo("Ready Player One?"));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        mainActivity.findViewById(R.id.findRestaurantsButton).performClick();
+
+        Intent expectedIntent = new Intent(mainActivity, RestaurantsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(mainActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
