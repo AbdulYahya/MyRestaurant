16fa50247aeeabedc28b2acdd45b0179b22f0ba5
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-12 21:12:53 -0700

    Added custom fonts. Both MainActivity Instrumentation tests pass

--- app/src/main/java/me/ayahya/aesirr/myrestaurant/MainActivity.java
+++ app/src/main/java/me/ayahya/aesirr/myrestaurant/MainActivity.java
@@ -1,23 +1,30 @@
 package me.ayahya.aesirr.myrestaurant;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText locationEditText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button findRestaurantButton;
+        TextView fontTextView;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fontTextView = findViewById(R.id.mainHeaderTextView);
+        Typeface caviarDreamsFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        fontTextView.setTypeface(caviarDreamsFont);
 
         locationEditText = findViewById(R.id.locationEditText);
         findRestaurantButton = findViewById(R.id.findRestaurantsButton);
